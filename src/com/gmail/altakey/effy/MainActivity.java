21896c5dac8dcac2e779398c575c9dc0a38bc596
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2011-09-25 00:29:31 +0900

    Removing unnecessary imports

--- src/com/gmail/altakey/effy/MainActivity.java
+++ src/com/gmail/altakey/effy/MainActivity.java
@@ -7,10 +7,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.content.Intent;
 import android.widget.Toast;
-import android.widget.SeekBar;
 import android.preference.PreferenceManager;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 
 public class MainActivity extends Activity
 {
