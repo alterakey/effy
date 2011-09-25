a93e65a8ba8bc7bb2b442c64707aa8a81b531125
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2011-09-26 00:39:02 +0900

    Citing base project more explicitly

--- src/com/gmail/altakey/effy/MainService.java
+++ src/com/gmail/altakey/effy/MainService.java
@@ -15,6 +15,26 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * Uses portions of code from the 光学☆迷彩 project, which
+ * is available from https://github.com/matsumo/kougaku-meisai .
+ * 
+ * Original copyright information:
+ * 
+ * Copyright (C) 2011 matsumo All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package com.gmail.altakey.effy;
 
 import java.util.List;
