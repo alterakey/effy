9374d59eafa2858d2dcd8777859c146bfb8645d8
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2011-10-01 18:49:59 +0900

    Experimentally handling touch event

--- src/com/gmail/altakey/effy/DrawActivity.java
+++ src/com/gmail/altakey/effy/DrawActivity.java
@@ -22,10 +22,14 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.MotionEvent;
 import android.content.Intent;
 import android.widget.Toast;
 import android.preference.PreferenceManager;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+
+import android.util.Log;
 
 public class DrawActivity extends Activity
 {
@@ -36,4 +40,32 @@ public class DrawActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.draw);
     }
+	
+	private void setPixel(float x, float y, int color)
+	{
+		Bitmap bitmap = Scribble.getInstance(1, 1).bitmap;
+		bitmap.setPixel(0, 0, color);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		int action = event.getAction() & MotionEvent.ACTION_MASK;
+		int actionIndex = (event.getAction() & MotionEvent.ACTION_POINTER_ID_MASK) >>> MotionEvent.ACTION_POINTER_ID_SHIFT;
+		
+		switch (action & MotionEvent.ACTION_MASK) 
+		{
+		case MotionEvent.ACTION_DOWN:
+		case MotionEvent.ACTION_POINTER_DOWN:
+		case MotionEvent.ACTION_MOVE:
+			Log.d("com.gmail.altakey.effy.DrawActivity", String.format("touched!: %d", action & MotionEvent.ACTION_MASK));
+			float x = event.getX(actionIndex);
+			float y = event.getY(actionIndex);
+			setPixel(x, y, 0xffffffff);
+			break;
+		default:
+			return false;
+		}
+		return true;
+	}
 }
